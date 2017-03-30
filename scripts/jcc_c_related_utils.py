e3b23d661a0fead12511458655f6ae1658e2fe53
Ziyue Yang <yzylivezh@hotmail.com>
2017-03-30 16:54:35 +0800

    code refinement

--- scripts/jcc_c_related_utils.py
+++ scripts/jcc_c_related_utils.py
@@ -1,18 +1,12 @@
 #coding=utf-8
 
-"""The prototype of the compiler for new jailhouse config file format
+"""C-Related Jailhouse Config Compiler Utils
 """
 
-import io
 import os
 import re
 import subprocess
-import struct
-import argparse
-import json
-import yaml
 import pycparser
-
 
 def include_c_headers(compiler_config_json, eliminate_gcc_attr=False):
     c_headers = compiler_config_json["c_headers"]
@@ -143,80 +137,3 @@ def get_macro_map(compiler_config_json):
     for macro_id in macro_map:
         print "%s %s" % (macro_id, macro_map[macro_id])
     return macro_map
-
-
-def gen_cell_bytes(config_yaml):
-    full_bytes = ""
-
-    # jailhouse_cell_desc
-    jailhouse_cell_desc = config_yaml["jailhouse_cell_desc"]
-    full_bytes += struct.pack("=6sH32sIIIIIIIIII",
-                              *[next(iter(x.items()))[1] for x in jailhouse_cell_desc])
-
-    # cpus
-    cpus = config_yaml["cpus"]
-    full_bytes += struct.pack("Q" * len(cpus), *cpus)
-
-    # mem_regions
-    mem_regions = config_yaml["mem_regions"]
-    for mem_region in mem_regions:
-        full_bytes += struct.pack("QQQQ", *[next(iter(x.items()))[1] for x in mem_region])
-
-    # cache_regions
-    cache_regions = config_yaml["cache_regions"]
-    for cache_region in cache_regions:
-        full_bytes += struct.pack("IIBBH", *[next(iter(x.items()))[1] for x in cache_region])
-
-    # pio_bitmap
-    pio_bitmap = config_yaml["pio_bitmap"]
-    bitmap_bytes = bytearray(struct.pack("B", pio_bitmap["default_value"]) * pio_bitmap["size"])
-    for hole in pio_bitmap["holes"]:
-        for idx in range(hole["begin"], hole["end"] + 1):
-            bitmap_bytes[idx] = struct.pack("B", hole["value"])
-    full_bytes += str(bitmap_bytes)
-
-    return full_bytes
-
-
-def run(compiler_config_json_path, cell_config_yaml_path):
-    """
-    parse config file and assemble config file
-    """
-    with open(os.path.abspath(compiler_config_json_path), "r") as compiler_config_json_file:
-        compiler_config_json = json.load(compiler_config_json_file)
-
-    get_struct_field_info(compiler_config_json)
-    get_macro_map(compiler_config_json)
-    """
-    with open(os.path.abspath(cell_config_yaml_path), "r") as cell_config_yaml_file:
-        cell_config_yaml = yaml.load(cell_config_yaml_file)
-        print cell_config_yaml
-        cell_bytes = gen_cell_bytes(cell_config_yaml)
-        with open(output_path, "wb") as output_file:
-            output_file.write(cell_bytes)
-    """
-
-def parse_args():
-    """
-    parse command line input
-    """
-    parser = argparse.ArgumentParser(description="Jailhouse config file compiler prototype")
-    parser.add_argument("-c", action="store", dest="compiler_config_json_path",
-                        required=True, help="path/to/compiler_config.json")
-    parser.add_argument("-y", action="store", dest="cell_config_yaml_path",
-                        required=True, help="path/to/cell_config.yaml")
-    options = parser.parse_args()
-    return options
-
-
-def main():
-    """
-    the main function
-    """
-    opts = parse_args()
-    run(opts.compiler_config_json_path, opts.cell_config_yaml_path)
-    return
-
-
-if __name__ == "__main__":
-    main()
